5287100c881ff8931864df35dc81f98b52a4a4ff
DawnM <dawn.cm@outlook.com>
2018-08-29 16:39:08 +0000

    add vending machine

--- byotest.py
+++ byotest.py
@@ -1,9 +1,9 @@
-def is_even(number):
-    return number % 2 == 0 # will return true or false accordlingly
+# def is_even(number):
+#     return number % 2 == 0 # will return true or false accordlingly
     
-def number_of_evens(numbers):
-    evens = sum([1 for n in numbers if is_even(n)])
-    return evens
+# def number_of_evens(numbers):
+#     evens = sum([1 for n in numbers if is_even(n)])
+#     return evens
     
 def test_are_equal(actual, expected):
     assert expected == actual, "Expected {0}, got {1}".format(expected, actual)
@@ -17,10 +17,10 @@ def test_is_in(collection, item):
 def test_not_in(collection, item):
     assert item not in collection, "{0} does contain {1}".format(collection, item)
 
-collection = [1, 2, 3, 4, 5]
+# collection = [1, 2, 3, 4, 5]
 
-test_are_equal(number_of_evens([1, 2, 3, 4, 5]), 2)
-test_not_equal(5, 2)
-test_is_in(collection, 3)
-test_not_in(collection, 6)
-print("All tests ran")
+# test_are_equal(number_of_evens([1, 2, 3, 4, 5]), 2)
+# test_not_equal(5, 2)
+# test_is_in(collection, 3)
+# test_not_in(collection, 6)
+# print("All tests ran")
